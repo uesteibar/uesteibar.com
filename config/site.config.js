29f7507a0cf051096d7ea2694cea1e2e1d03dec1
uesteibar <uesteibar@gmail.com>
2020-05-24 08:32:55 +0200

    Add about me section

--- config/site.config.js
+++ config/site.config.js
@@ -9,13 +9,13 @@ if (!ROOT) {
 
 const config = {
   // Your website's name, used for favicon meta tags
-  site_name: 'Static Site Boilerplate',
+  site_name: 'uesteibar',
 
   // Your website's description, used for favicon meta tags
-  site_description: 'A modern boilerplate for static website development',
+  site_description: 'I\'m Unai, a Software Engineer based in Barcelona. I love open source, taking photos and cooking. I build web stuff at Sketch.',
 
   // Your website's URL, used for sitemap
-  site_url: 'http://staticsiteboilerplate.com',
+  site_url: 'https://uesteibar.com',
 
   // Google Analytics tracking ID (leave blank to disable)
   googleAnalyticsUA: '',
